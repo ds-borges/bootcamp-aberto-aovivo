0726e5647c2c891f9e598f9a023aebb36223724f
ds-borges <diego97sousa@gmail.com>
2026-01-02 17:22:17 -0300

    build: add dbt conection

---
+++ dags/dag_dbt.py
@@ -0,0 +1,25 @@
+from airflow.decorators import dag, task
+from airflow.providers.dbt.cloud.hooks.dbt import DbtCloudHook, DbtCloudJobRunStatus
+from airflow.providers.dbt.cloud.operators.dbt import DbtCloudRunJobOperator
+from datetime import datetime
+
+DBT_CLOUD_CONN_ID = "dbt-conn"
+JOB_ID = "70471823545829"
+@dag(
+    dag_id="dbt_dag",
+    description="minha etl braba",
+    schedule="@daily", #Cada 5 Min
+    catchup=False #backfill
+)
+def running_dbt_cloud():
+    rodar_dbt = DbtCloudRunJobOperator(
+        task_id = "rodar_dbt",
+        dbt_cloud_conn_id=DBT_CLOUD_CONN_ID,
+        job_id=JOB_ID,
+        check_interval=60,
+        timeout=360,
+    )
+
+    rodar_dbt
+
+running_dbt_cloud()
