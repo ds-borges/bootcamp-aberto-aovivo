669224fded755306febd4bd60765b09dc5ca8dd0
ds-borges <diego97sousa@gmail.com>
2026-01-02 22:02:44 -0300

    feat: fix imports HttpOperator

--- dags/dag_airbyte.py
+++ dags/dag_airbyte.py
@@ -1,5 +1,5 @@
 from airflow.decorators import dag
-from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.providers.http.operators.http import HttpOperator
 from airflow.providers.http.sensors.http import HttpSensor
 from airflow.models import Variable
 import json
@@ -8,10 +8,10 @@ from datetime import datetime
 AIRBYTE_CONNETCION_ID = Variable.get("AIRBYTE_GOOGLE_POSTGRES_CONNECTION_ID")
 API_KEY = f'Bearer {Variable.get("AIRBYTE_API_TOKEN")}'
 
-@dag(dag_id="airbyte_dag_google_sheets_postgres",start_date=datetime(2024, 4, 18), schedule_interval="@daily", catchup=False)
+@dag(start_date=datetime(2024, 4, 18), schedule_interval="@daily", catchup=False)
 def running_airbyte():
 
-    start_airbyte_sync = SimpleHttpOperator(
+    start_airbyte_sync = HttpOperator(
         task_id='start_airbyte_sync',
         http_conn_id='airbyte',
         endpoint=f'/v1/jobs',
